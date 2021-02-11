0465e075a886bbf5f14ceab8b11c487c6033f7f9
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2021-02-11 18:23:24 -0300

    Updated dashboard screen

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +40,87 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento à seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/40833512?s=460&u=67631b166ec3d929e67f32e7ae0172e3dcbac913&v=4"
+                alt="Rafael"
+              />
+
+              <strong>Rafael Tessarolo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40833512?s=460&u=67631b166ec3d929e67f32e7ae0172e3dcbac913&v=4"
+                  alt="Rafael"
+                />
+                <strong>Rafael Tessarolo</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40833512?s=460&u=67631b166ec3d929e67f32e7ae0172e3dcbac913&v=4"
+                  alt="Rafael"
+                />
+                <strong>Rafael Tessarolo</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                14:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40833512?s=460&u=67631b166ec3d929e67f32e7ae0172e3dcbac913&v=4"
+                  alt="Rafael"
+                />
+                <strong>Rafael Tessarolo</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
